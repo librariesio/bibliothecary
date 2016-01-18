968dc72ca28dfa46a57dbed4572c3bc500f5e282
Andrew Nesbitt <andrewnez@gmail.com>
2016-01-18 19:03:54 +0000

    Added shrink-wrap support

--- lib/bibliothecary/npm.rb
+++ lib/bibliothecary/npm.rb
@@ -3,6 +3,11 @@ require 'json'
 module Bibliothecary
   class NPM
     def self.analyse(folder_path, file_list)
+      [analyse_package_json(folder_path, file_list),
+      analyse_shrinkwrap(folder_path, file_list)]
+    end
+
+    def self.analyse_package_json(folder_path, file_list)
       path = file_list.find{|path| path.gsub(folder_path, '').gsub(/^\//, '').match(/^package\.json$/) }
       return unless path
 
@@ -15,18 +20,40 @@ module Bibliothecary
       }
     end
 
-    def self.parse_manifest(manifest)
+    def self.analyse_shrinkwrap(folder_path, file_list)
+      path = file_list.find{|path| path.gsub(folder_path, '').gsub(/^\//, '').match(/^npm-shrinkwrap\.json$/) }
+      return unless path
+
+      manifest = JSON.parse File.open(path).read
+
+      {
+        platform: 'npm',
+        path: path,
+        dependencies: parse_shrinkwrap(manifest)
+      }
+    end
+
+    def self.parse_shrinkwrap(manifest)
       manifest.fetch('dependencies',[]).map do |name, requirement|
         {
           name: name,
-          requirement: requirement,
+          requirement: requirement["version"],
           type: 'runtime'
         }
-      end + manifest.fetch('devDependencies',[]).map do |name, requirement|
+      end
+    end
+
+    def self.parse_manifest(manifest)
+      map_dependencies(manifest, 'dependencies', 'runtime') +
+      map_dependencies(manifest, 'devDependencies', 'development')
+    end
+
+    def self.map_dependencies(hash, key, type)
+      hash.fetch(key,[]).map do |name, requirement|
         {
           name: name,
           requirement: requirement,
-          type: 'development'
+          type: type
         }
       end
     end
