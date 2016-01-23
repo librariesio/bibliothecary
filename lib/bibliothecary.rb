eaea11de5c88f24720966efcbb187f97b938c26e
Andrew Nesbitt <andrewnez@gmail.com>
2016-01-23 15:11:52 +0000

    Added packagist support

--- lib/bibliothecary.rb
+++ lib/bibliothecary.rb
@@ -1,18 +1,20 @@
 require "bibliothecary/version"
 require "bibliothecary/npm"
 require "bibliothecary/bower"
+require "bibliothecary/packagist"
 
 module Bibliothecary
   def self.analyse(path)
     cmd = `find #{path} -type f | grep -vE "#{ignored_files}"`
     file_list = cmd.split("\n")
-    package_managers.map{|pm| pm.analyse(path, file_list) }.compact.flatten
+    package_managers.map{|pm| pm.analyse(path, file_list) }.flatten.compact
   end
 
   def self.package_managers
     [
       NPM,
-      Bower
+      Bower,
+      Packagist
     ]
   end
 
