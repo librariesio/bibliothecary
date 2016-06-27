d48f5eca1a0f9b99b565351d2667f8a6b2b51ecb
Andrew Nesbitt <andrewnez@gmail.com>
2016-06-27 13:29:41 +0100

    Added julia spec

---
+++ spec/parsers/julia_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Bibliothecary::Parsers::Julia do
+  it 'has a platform name' do
+    expect(Bibliothecary::Parsers::Julia::PLATFORM_NAME).to eq('julia')
+  end
+
+  it 'parses dependencies from REQUIRE' do
+    file = load_fixture('REQUIRE')
+
+    expect(Bibliothecary::Parsers::Julia.parse('REQUIRE', file)).to eq([
+      {:name=>"julia", :requirement=>"0.3", :type=>"runtime"},
+      {:name=>"Codecs", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Colors", :requirement=>"0.3.4", :type=>"runtime"},
+      {:name=>"Compat", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Compose", :requirement=>"0.3.11", :type=>"runtime"},
+      {:name=>"Contour", :requirement=>"*", :type=>"runtime"},
+      {:name=>"DataFrames", :requirement=>"0.4.2", :type=>"runtime"},
+      {:name=>"DataStructures", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Dates", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Distributions", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Hexagons", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Iterators", :requirement=>"0.1.5", :type=>"runtime"},
+      {:name=>"JSON", :requirement=>"*", :type=>"runtime"},
+      {:name=>"KernelDensity", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Loess", :requirement=>"*", :type=>"runtime"},
+      {:name=>"Showoff", :requirement=>"0.0.3", :type=>"runtime"},
+      {:name=>"StatsBase", :requirement=>"*", :type=>"runtime"}])
+  end
+end
