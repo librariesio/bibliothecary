86a43e9d6b3e2c9e84da149463d44dd30b2f7952
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-20 11:08:14 +0100

    Switch swift parsing ip to domain name

--- lib/bibliothecary/parsers/swift.rb
+++ lib/bibliothecary/parsers/swift.rb
@@ -13,7 +13,7 @@ module Bibliothecary
       end
 
       def self.parse_package_swift(manifest)
-        response = Typhoeus.post("http://192.241.154.173/to-json", body: manifest)
+        response = Typhoeus.post("http://swiftpm.honza.tech/to-json", body: manifest)
         json = JSON.parse(response.body)
         json["dependencies"].map do |dependency|
           name = dependency['url'].gsub(/^https?:\/\//, '').gsub(/\.git$/,'')
