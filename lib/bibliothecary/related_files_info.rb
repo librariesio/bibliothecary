884ca3887e1ce3c0979d90d8c250130fb129841a
jsonperl <jason.a.pearl@gmail.com>
2019-04-22 09:21:49 -0400

    relative_folder_path_for_related_files_info

--- lib/bibliothecary/related_files_info.rb
+++ lib/bibliothecary/related_files_info.rb
@@ -1,6 +1,7 @@
 module Bibliothecary
   class RelatedFilesInfo
     attr_reader :folder_path
+    attr_reader :relative_folder_path
     attr_reader :platform
     attr_reader :manifests
     attr_reader :lockfiles
@@ -21,6 +22,7 @@ module Bibliothecary
       package_manager = file_infos.first.package_manager
       @platform = package_manager.platform_name
       @folder_path = File.join(file_infos.first.folder_path, File.dirname(file_infos.first.relative_path))
+      @relative_folder_path = File.dirname(file_infos.first.relative_path)
       @manifests = file_infos.select { |info| package_manager.determine_kind_from_info(info) == "manifest" }.map { |info| File.basename(info.relative_path) }
       @lockfiles = file_infos.select { |info| package_manager.determine_kind_from_info(info) == "lockfile" }.map { |info| File.basename(info.relative_path) }
     end
