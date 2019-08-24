c0819fdde42f7a903d35f6e88a138da43cdcc728
Tyrel Souza <923113+tyrelsouza@users.noreply.github.com>
2019-08-23 23:00:48 -0400

    oops wrong method

--- lib/bibliothecary/parsers/conda.rb
+++ lib/bibliothecary/parsers/conda.rb
@@ -21,21 +21,10 @@ module Bibliothecary
         }
       end
 
+      def self.parse_conda_environment(file_contents)
+        manifest = call_conda_parser_web(file_contents)
 
-      def self.call_conda_parser_web(file_contents)
-        host = Bibliothecary.configuration.conda_parser_host
-        response = Typhoeus.post(
-          "#{host}/parse",
-          headers: {
-              ContentType: 'multipart/form-data'
-          },
-          # TODO: Can we ever get the filename from the method call from `match_filename` in the future?
-          # Would be nice to be able to send the file name that clients use for their environment to be able to log better.
-          body: {file: file_contents, filename: 'environment.yml'}
-        )
-        raise Bibliothecary::RemoteParsingError.new("Http Error #{response.response_code} when contacting: #{host}/parse", response.response_code) unless response.success?
-
-        JSON.parse(response.body)
+        Hash[FILE_KINDS.collect { |kind| [kind, map_dependencies(manifest, kind, "runtime")] }]
       end
 
       # Overrides Analyser.analyse_contents_from_info
@@ -53,16 +42,24 @@ module Bibliothecary
       rescue Bibliothecary::RemoteParsingError => e
         Bibliothecary::Analyser::create_error_analysis(platform_name, info.relative_path, "runtime", e.message)
       end
-    end
     
-    private
+      private
 
-    def self.parse_conda_environment(file_contents)
-      manifest = call_conda_parser_web(file_contents)
+      def self.call_conda_parser_web(file_contents)
+        host = Bibliothecary.configuration.conda_parser_host
+        response = Typhoeus.post(
+          "#{host}/parse",
+          headers: {
+              ContentType: 'multipart/form-data'
+          },
+          # TODO: Can we ever get the filename from the method call from `match_filename` in the future?
+          # Would be nice to be able to send the file name that clients use for their environment to be able to log better.
+          body: {file: file_contents, filename: 'environment.yml'}
+        )
+        raise Bibliothecary::RemoteParsingError.new("Http Error #{response.response_code} when contacting: #{host}/parse", response.response_code) unless response.success?
 
-      Hash[FILE_KINDS.collect { |kind| [kind, map_dependencies(manifest, kind, "runtime")] }]
+        JSON.parse(response.body)
+      end
     end
-
-
   end
 end
