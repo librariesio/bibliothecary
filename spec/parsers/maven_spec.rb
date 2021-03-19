9001da4c6c0ae840818aa1e80cd3ae4bd841ecf8
Matt Ellis <mellis@tidelift.com>
2021-03-19 13:00:02 -0400

    remove binding

--- spec/parsers/maven_spec.rb
+++ spec/parsers/maven_spec.rb
@@ -397,7 +397,6 @@ RSpec.describe Bibliothecary::Parsers::Maven do
     it "parses dependencies from maven-dependency-tree files" do
       contents = load_fixture("maven-dependency-tree.txt")
       output = described_class.parse_maven_tree(contents)
-      binding.pry
       expect(output.count).to eq 315
       expect(output.find {|item| item[:name] == "org.apache.commons:commons-lang3"}[:requirement]).to eq "3.8.1"
     end
