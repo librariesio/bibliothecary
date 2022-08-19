112f991efd58de5a2feb5ac1680a027b4dce8bd1
Tieg Zaharia <tieg@tidelift.com>
2022-08-19 11:42:36 -0700

    Simplify spec

--- spec/parsers/maven_spec.rb
+++ spec/parsers/maven_spec.rb
@@ -498,7 +498,7 @@ RSpec.describe Bibliothecary::Parsers::Maven do
     end
 
     it "properly interpolates self-referential project lines" do
-      gradle_dependencies_q = <<-GRADLE
+      gradle_dependencies_out = <<-GRADLE
 ------------------------------------------------------------
 Project ':submodules:test'
 ------------------------------------------------------------
@@ -506,7 +506,6 @@ Project ':submodules:test'
 compileClasspath - Compile classpath for source set 'main'.
 +--- project :
 |    \\--- io.qameta.allure:allure-test-filter:2.18.1 (*)
-\\--- org.slf4j:jcl-over-slf4j -> 1.7.36 (*)
 
 (c) - dependency constraint
 (*) - dependencies omitted (listed previously)
@@ -514,7 +513,7 @@ compileClasspath - Compile classpath for source set 'main'.
 A web-based, searchable dependency report is available by adding the --scan option.
 GRADLE
 
-      expect(described_class.parse_gradle_resolved(gradle_dependencies_q)).to eq [{
+      expect(described_class.parse_gradle_resolved(gradle_dependencies_out)).to eq [{
         name: "internal:submodules:test",
         requirement: "1.0.0",
         type: "compileClasspath"
@@ -522,11 +521,6 @@ GRADLE
       {
         name: "io.qameta.allure:allure-test-filter",
         requirement: "2.18.1",
-        type: "compileClasspath"
-      },
-      {
-        name: "org.slf4j:jcl-over-slf4j",
-        requirement: "1.7.36",
         type: "compileClasspath"
       }]
     end
