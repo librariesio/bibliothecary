145d35cf54b799b9f6a2799739b24b99430f2a2b
Tieg Zaharia <tieg@tidelift.com>
2022-08-19 11:31:45 -0700

    Track the current project name while parsing gradle to use later.
    Sometimes projects can be listed as deps with empty names that
    imply their project name, e.g. "Project: ':my:project'"

--- spec/parsers/maven_spec.rb
+++ spec/parsers/maven_spec.rb
@@ -497,6 +497,40 @@ RSpec.describe Bibliothecary::Parsers::Maven do
 
     end
 
+    it "properly interpolates self-referential project lines" do
+      gradle_dependencies_q = <<-GRADLE
+------------------------------------------------------------
+Project ':submodules:test'
+------------------------------------------------------------
+
+compileClasspath - Compile classpath for source set 'main'.
++--- project :
+|    \\--- io.qameta.allure:allure-test-filter:2.18.1 (*)
+\\--- org.slf4j:jcl-over-slf4j -> 1.7.36 (*)
+
+(c) - dependency constraint
+(*) - dependencies omitted (listed previously)
+
+A web-based, searchable dependency report is available by adding the --scan option.
+GRADLE
+
+      expect(described_class.parse_gradle_resolved(gradle_dependencies_q)).to eq [{
+        name: "internal:submodules:test",
+        requirement: "1.0.0",
+        type: "compileClasspath"
+      },
+      {
+        name: "io.qameta.allure:allure-test-filter",
+        requirement: "2.18.1",
+        type: "compileClasspath"
+      },
+      {
+        name: "org.slf4j:jcl-over-slf4j",
+        requirement: "1.7.36",
+        type: "compileClasspath"
+      }]
+    end
+
     it "properly handles no version to resolved version syntax" do
       no_version_to_version = "\\--- org.springframework.security:spring-security-test -> 5.2.2.RELEASE"
       expect(described_class
