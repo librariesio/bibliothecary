0a2311837416868fb58ff150244a7cc33be8e371
Emmanu Varghese <emmanu.varghese@bacancy.com>
2023-07-24 18:58:46 +0530

    log gemfile content

--- lib/bibliothecary/multi_parsers/bundler_like_manifest.rb
+++ lib/bibliothecary/multi_parsers/bundler_like_manifest.rb
@@ -17,6 +17,18 @@ module Bibliothecary
           })
         end.uniq
       end
+
+      # Method to extract gems with a pattern from the Gemfile content
+      def extract_gems_by_pattern(pattern, gemfile_content)
+        gems = []
+        puts "Gemfile Content from gem"
+        puts gemfile_content
+        gemfile_content.scan(pattern) do |gem_name, _require_option|
+          gems << { name: gem_name.strip, type: :runtime }
+        end
+
+        gems
+      end
     end
   end
 end
