b94b107b76eca543abf0f6156999a68cae04e4f5
Jeremy Katz <jeremy@tidelift.com>
2024-11-20 17:42:50 +0000

    Move yarn parsing to be fully in process
    This stops using yarn-parser and instead does parsing locally
    for yarn.lock (v1 and v2)

--- lib/bibliothecary/configuration.rb
+++ lib/bibliothecary/configuration.rb
@@ -5,7 +5,6 @@ module Bibliothecary
     attr_accessor :carthage_parser_host
     attr_accessor :clojars_parser_host
     attr_accessor :mix_parser_host
-    attr_accessor :yarn_parser_host
     attr_accessor :conda_parser_host
     attr_accessor :swift_parser_host
     attr_accessor :cabal_parser_host
@@ -16,7 +15,6 @@ module Bibliothecary
       @carthage_parser_host = "https://carthage.libraries.io"
       @clojars_parser_host  = "https://clojars.libraries.io"
       @mix_parser_host      = "https://mix.libraries.io"
-      @yarn_parser_host     = "https://yarn-parser.libraries.io"
       @conda_parser_host    = "https://conda-parser.libraries.io"
       @swift_parser_host    = "http://swift.libraries.io"
       @cabal_parser_host    = "http://cabal.libraries.io"
