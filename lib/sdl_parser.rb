0e9ca3108037604c4192df622f9fcf6189862f51
Tieg Zaharia <tieg@tidelift.com>
2025-01-29 16:16:36 -0700

    Populate source field in Dub

--- lib/sdl_parser.rb
+++ lib/sdl_parser.rb
@@ -2,17 +2,19 @@ require "sdl4r"
 
 class SdlParser
   attr_reader :contents, :type
-  def initialize(type, contents)
+  def initialize(type, contents, source=nil)
     @contents = contents
     @type = type || "runtime"
+    @source = source
   end
 
-  def dependencies
+def dependencies
     parse.children("dependency").inject([]) do |deps, dep|
       deps.push(Bibliothecary::Dependency.new(
         name: dep.value,
         requirement: dep.attribute("version") || ">= 0",
         type: type,
+        source: @source,
       ))
     end.uniq
   end
